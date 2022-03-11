84f2dddbe36a69be692db8e04d555863532199f0
Eveleen <we820731@gmail.com>
2022-03-11 06:04:44 +0000

    test view single question

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/questions', 'QuestionsController@index');
+Route::get('/questions/{question}', 'QuestionsController@show');
--- tests/Feature/ViewQuestionsTest.php
+++ tests/Feature/ViewQuestionsTest.php
@@ -2,11 +2,14 @@
 
 namespace Tests\Feature;
 
+use App\Models\Question;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ViewQuestionsTest extends TestCase
 {
+    use RefreshDatabase;
+
     /** @test */
     public function user_can_view_questions()
     {
@@ -16,5 +19,20 @@ class ViewQuestionsTest extends TestCase
         $test = $this->get('/questions');
         // 2. 正常返回 200
         $test->assertStatus(200);
+    }
+
+    /** @test */
+    public function user_can_view_a_single_question()
+    {
+        // 1. 新增一個問題
+        $question = Question::factory()->create();
+
+        // 2. 訪問連結
+        $test = $this->get('/questions/' . $question->id);
+
+        // 3. 應該看到問題內容
+        $test->assertStatus(200)
+            ->assertSee($question->title)
+            ->assertSee($question->content);
     }
 }
