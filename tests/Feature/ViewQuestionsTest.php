71ab2ba8e5ff130ed64782530c57c3f1b466e8ab
Eveleen <we820731@gmail.com>
2022-03-11 05:26:23 +0000

    rename tests

--- tests/Feature/ViewQuestionsTest.php
+++ tests/Feature/ViewQuestionsTest.php
@@ -7,15 +7,14 @@ use Tests\TestCase;
 
 class ViewQuestionsTest extends TestCase
 {
-    public function testUserCanViewQuestions()
+    /** @test */
+    public function user_can_view_questions()
     {
-
         // 0. 拋出異常
         $this->withoutExceptionHandling();
-        // 1. 假設 /questions 路由存在
-        // 2. 跳轉至 /questions
+        // 1. 跳轉至 /questions
         $test = $this->get('/questions');
-        // 3. 正常返回 200
+        // 2. 正常返回 200
         $test->assertStatus(200);
     }
 }
