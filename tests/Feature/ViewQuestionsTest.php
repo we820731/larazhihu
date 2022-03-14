ce80536e2e2effdb97994deabafd6f033a1a2fb1
Eveleen <we820731@gmail.com>
2022-03-14 03:41:05 +0000

    question published_at attribute debug

--- tests/Feature/ViewQuestionsTest.php
+++ tests/Feature/ViewQuestionsTest.php
@@ -23,21 +23,6 @@ class ViewQuestionsTest extends TestCase
     }
 
     /** @test */
-    public function user_can_view_a_single_question()
-    {
-        // 1. 新增一個問題
-        $question = Question::factory()->create();
-
-        // 2. 訪問連結
-        $test = $this->get('/questions/' . $question->id);
-
-        // 3. 應該看到問題內容
-        $test->assertStatus(200)
-            ->assertSee($question->title)
-            ->assertSee($question->content);
-    }
-
-    /** @test */
     public function user_can_view_a_published_question()
     {
         $question = Question::factory()->create(['published_at' => Carbon::parse('-1 week')]);
