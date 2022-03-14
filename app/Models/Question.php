9723de3c8fa08fe09c12a22e842dc466d4baef50
Eveleen <we820731@gmail.com>
2022-03-14 08:35:08 +0000

    refactor to markAsBestAnswer()

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -20,4 +20,11 @@ class Question extends Model
     {
         return $this->hasMany(Answer::class);
     }
+
+    public function markAsBestAnswer($answer)
+    {
+        $this->update([
+            'best_answer_id' => $answer->id
+        ]);
+    }
 }
