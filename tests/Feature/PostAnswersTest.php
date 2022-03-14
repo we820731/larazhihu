0ba71ded1a1d8cdc8f8b7ad08ff42f4757b49951
Eveleen <we820731@gmail.com>
2022-03-14 04:05:02 +0000

    post answer

--- tests/Feature/PostAnswersTest.php
+++ tests/Feature/PostAnswersTest.php
@@ -13,10 +13,10 @@ class PostAnswersTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    public function user_can_post_an_answer_to_a_question()
+    public function user_can_post_an_answer_to_a_published_question()
     {
         // 假測已存在某個問題
-        $question = Question::factory()->create();
+        $question = Question::factory()->published()->create();
         $user = User::factory()->create();
 
         // 我們要觸發某個路由
@@ -30,5 +30,23 @@ class PostAnswersTest extends TestCase
         $answer = $question->answers()->where('user_id', $user->id)->first();
         $this->assertNotNull($answer);
         $this->assertEquals(1, $question->answers()->count());
+    }
+
+    /** @test */
+    public function can_not_post_an_answer_to_an_unpublished_question()
+    {
+        $question = Question::factory()->unpublished()->create();
+        $user = User::factory()->create();
+
+        $response = $this->withExceptionHandling()
+            ->post("/questions/{$question->id}/answers", [
+                'user_id' => $user->id,
+                'content' => 'This is an answer.'
+            ]);
+
+        $response->assertStatus(404);
+
+        $this->assertDatabaseMissing('answers', ['question_id' => $question->id]);
+        $this->assertEquals(0, $question->answers()->count());
     }
 }
