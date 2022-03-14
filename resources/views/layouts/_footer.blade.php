18c98555361548917aabd3338c143bad9f65c2c0
Eveleen <we820731@gmail.com>
2022-03-14 10:09:38 +0000

    layouts

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      刻意練習，每日精進!
+    </p>
+
+    <p class="float-right"><a href="https://learnku.com/courses/laravel-testing" target="_blank">Laravel TDD</a></p>
+  </div>
+</footer>
