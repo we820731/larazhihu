ca32ffd889effe3a00b4579bfe0ff2df924fc571
Eveleen <we820731@gmail.com>
2022-03-15 01:05:30 +0000

    update header text

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -11,14 +11,14 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <!-- Left Side Of Navbar -->
       <ul class="navbar-nav mr-auto">
-        <li class="nav-item"><a class="nav-link" href="/questions">发现</a></li>
+        <li class="nav-item"><a class="nav-link" href="/questions">發現</a></li>
       </ul>
 
       <!-- Right Side Of Navbar -->
       <ul class="navbar-nav navbar-right">
         <!-- Authentication Links -->
         @guest
-          <li class="nav-item"><a class="btn-sm btn btn-outline-primary fs-09" href="{{ route('login') }}">登录</a></li>
+          <li class="nav-item"><a class="btn-sm btn btn-outline-primary fs-09" href="{{ route('login') }}">登入</a></li>
           <li class="nav-item"><a class="ml-3 btn-sm btn btn-primary fs-09" href="{{ route('register') }}">加入知乎</a></li>
         @else
           <li class="nav-item dropdown">
@@ -26,12 +26,12 @@
               {{ Auth::user()->name }}
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">个人中心</a>
+              <a class="dropdown-item" href="#">個人中心</a>
               <div class="dropdown-divider"></div>
               <a class="dropdown-item" id="logout" href="#">
                 <form action="{{ route('logout') }}" method="POST">
                   {{ csrf_field() }}
-                  <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
+                  <button class="btn btn-block btn-danger" type="submit" name="button">登出</button>
                 </form>
               </a>
             </div>
